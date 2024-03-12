110559dd624ecf960ec556600f38943152021361
Shivani Tiwari <116451611+SheWe786@users.noreply.github.com>
2024-03-12 19:10:55 +0530

    Update-march

---
+++ src/components/Navbar/SearchBar.jsx
@@ -0,0 +1,33 @@
+import React, { useContext } from 'react'
+import { DarkMode, UserLogedIn } from '../../App';
+import { useNavigate } from 'react-router-dom';
+import { CiSearch } from "react-icons/ci";
+
+const SearchBar = () => {
+  const navigate = useNavigate();
+  const { logedIn, setSearchTerm } = useContext(UserLogedIn);
+  const { darkMode } = useContext(DarkMode);
+
+
+  const onChange = (e) => {
+    setSearchTerm(e.target.value);
+  }
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    navigate('/searchPage')
+    setSearchTerm("");
+  }
+
+
+
+  return (
+    <form className="search-bar" onSubmit={handleOnSubmit}>
+
+      <input className={`search-input-${(logedIn && !darkMode) ? "light" : "dark"}`}
+        placeholder='Search Reddit' onChange={onChange} />
+    </form>
+  )
+}
+
+export default SearchBar;
